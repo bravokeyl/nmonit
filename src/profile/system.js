d5c1e14687e67d9a84ecb47faa97da84b919d145
bravokeyl <bravokeyl@gmail.com>
2017-11-06 14:00:03 +0530

    Profile system view

--- src/profile/system.js
+++ src/profile/system.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import List, { ListItem, ListItemText, ListSubheader } from 'material-ui/List';
 import { withStyles } from 'material-ui/styles';
 
 const styles = theme => ({
@@ -34,7 +36,21 @@ class System extends Component {
       <div className={classes.root}>
         <Grid container spacing={0}>
           <Grid item xs={12} sm={12}>
-            System
+            <List>
+              <ListItem>
+                PV system name: Khammam Shyamala Hospital
+              </ListItem>
+              <ListItem>
+                Time zone : UTC+5:30
+              </ListItem>
+              <ListItem>
+                Installation Date : Sep 28th 2017
+              </ListItem>
+              <ListItem>
+                Currency: INR
+              </ListItem>
+            </List>
+            <Typography type="subheading">Address:</Typography>
           </Grid>
         </Grid>
       </div>
