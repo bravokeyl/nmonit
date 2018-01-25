60d6ac54a07648de6d4a2da5744642c1b189f383
bravokeyl <bravokeyl@gmail.com>
2018-01-25 16:30:43 +0530

    Lint for login view :construction:

--- src/login/index.js
+++ src/login/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
@@ -9,21 +10,19 @@ import { authenticateUser } from '../aws/cognito';
 import { bkLog } from '../common/utils';
 
 import bg from './bg.jpg';
+
 const styles = theme => ({
   root: {
     width: '100vw',
     minHeight: '100vh',
-    // background: "linear-gradient(to right, #fd651a 0%,#fb6529 24%,#f3635c 76%,#f1636c 100%)",
-    // background: 'aliceblue',
-    background: 'url('+bg+')',
-    // background: '#FF5722',
+    background: `url(${bg})`,
   },
   flex: {
     display: 'flex',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    minHeight: '100vh'
+    minHeight: '100vh',
   },
   container: {
     padding: '60px 50px',
@@ -39,49 +38,42 @@ const styles = theme => ({
     marginRight: theme.spacing.unit,
     [theme.breakpoints.up('sm')]: {
       width: 400,
-    }
+    },
   },
   formControl: {
     margin: theme.spacing.unit,
   },
 });
-// const signOut = () => {
-//   var auth2 = window.gapi.auth2.getAuthInstance();
-//   auth2.signOut().then(function () {
-//     console.log('User signed out.');
-//   });
-// };
+
 class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      isLoggedin: false,
       email: '',
       password: '',
-      showPassword: false,
       loading: false,
-    }
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-  handleChange = name => event => {
+  componentDidMount() {
+    bkLog('Login component Did mount');
+  }
+  handleChange = name => (event) => {
     this.setState({
       [name]: event.target.value,
     });
   };
-  handleSubmit = (e) => {
-    this.setState({ loading: true })
+  handleSubmit = () => {
+    this.setState({ loading: true });
     authenticateUser(this.state.email, this.state.password, (err, result) => {
       if (err) {
-        console.error("Authenictation error:",err)
-        this.setState({ loading: false })
+        console.error('Authenictation error:', err);
+        this.setState({ loading: false });
         return;
       }
-      this.props.authHandler(result,this.props.history);
+      this.props.authHandler(result, this.props.history);
     });
-  }
-  componentDidMount(){
-    bkLog("Login component Did mount");
   }
 
   render() {
@@ -92,45 +84,42 @@ class Login extends Component {
           <Grid item xs={12} sm={12}>
             <div className={classes.flex}>
               {
-                (<form className={classes.container} noValidate >
-                  <TextField
-                    required
-                    id="email"
-                    label="Email"
-                    className={classes.textField}
-                    value={this.state.email}
-                    onChange={this.handleChange('email')}
-                    margin="normal"
-                    autoComplete="username email"
-                  />
-                  <br/>
-                  <TextField
-                    required
-                    id="password"
-                    label="Password"
-                    autoComplete="current-password"
-                    className={classes.textField}
-                    margin="normal"
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.handleChange('password')}
-                  />
-                  <br/>
-                  <Button raised className={classes.button} disabled={this.state.loading} color="primary"
-                    onClick={this.handleSubmit}
+                (
+                  <form className={classes.container} noValidate >
+                    <TextField
+                      required
+                      id="email"
+                      label="Email"
+                      className={classes.textField}
+                      value={this.state.email}
+                      onChange={this.handleChange('email')}
+                      margin="normal"
+                      autoComplete="username email"
+                    />
+                    <br />
+                    <TextField
+                      required
+                      id="password"
+                      label="Password"
+                      autoComplete="current-password"
+                      className={classes.textField}
+                      margin="normal"
+                      type="password"
+                      value={this.state.password}
+                      onChange={this.handleChange('password')}
+                    />
+                    <br />
+                    <Button
+                      raised
+                      className={classes.button}
+                      disabled={this.state.loading}
+                      color="primary"
+                      onClick={this.handleSubmit}
                     >
-                    Login
-                  </Button>
-                  {
-                  // <br/>
-                  // <div className="g-signin2" data-onsuccess="signinCallback"></div>
-                  // <Button raised className={classes.button} color="primary"
-                  //   onClick={signOut}
-                  //   >
-                  //   Sign out
-                  // </Button>
-                  }
-                </form>)
+                      Login
+                    </Button>
+                  </form>
+                )
               }
             </div>
           </Grid>
@@ -140,4 +129,8 @@ class Login extends Component {
   }
 }
 
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+  authHandler: PropTypes.func.isRequired,
+};
 export default withStyles(styles)(Login);
