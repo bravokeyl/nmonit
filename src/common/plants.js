d85fcebf6c480cd442a0df469e0ab0b7e6c10ec2
bravokeyl <bravokeyl@gmail.com>
2018-02-01 13:45:43 +0530

    Add PV Systems constan for now :bento:

---
+++ src/common/plants.js
@@ -0,0 +1,6 @@
+const PVSystems = [
+  { name: 'Shyamala Hospital', location: 'Wyra Road, Khammam' },
+  { name: 'Harvest School', location: 'Pakabanda Bazar, Khammam' },
+  { name: 'Poly Clinic', location: 'Khammam, Telangana' },
+];
+export default PVSystems;
